2bd59b0f060cedc7045f040e8b7d452c1a3ac11b
codyw9524 <codyw9524@gmail.com>
2017-09-21 11:51:22 -0500

    initial commit

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { NoteNewComponent } from './note-new/note-new.component';
+import { NoteComponent } from './note/note.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: NoteComponent,
+    children: []
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -3,18 +3,30 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { NoteService } from './note.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoteComponent } from './note/note.component';
+import { NoteNewComponent } from './note-new/note-new.component';
+import { NoteListComponent } from './note-list/note-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteComponent,
+    NoteNewComponent,
+    NoteListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    NoteService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
